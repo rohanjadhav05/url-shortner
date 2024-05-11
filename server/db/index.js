282ade116567cfd3e6ce33a406d19aacd90a75e9
rohanjadhav05 <rohan.jadhav511@gmail.com>
2024-05-11 20:12:15 +0530

    changed config file - 02

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
+  host: '127.0.0.1',
   user: 'root',
   password: '',
   database: 'url_shortner',
