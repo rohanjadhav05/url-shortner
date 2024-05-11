a244f67c68a09d535dcf99f3ef7c5100b574d166
rohanjadhav05 <rohan.jadhav511@gmail.com>
2024-05-11 20:20:14 +0530

    added port in configuration file

--- server/db/index.js
+++ server/db/index.js
@@ -9,7 +9,7 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  socketPath: '/var/run/mysqld/mysqld.sock'
+  port: '3306'
 });
 
 module.exports = pool;
