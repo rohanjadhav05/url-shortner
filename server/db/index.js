8b3fbc4a358a2bcf8423b36aee1843487f731cc0
rohanjadhav05 <rohan.jadhav511@gmail.com>
2024-05-12 11:01:50 +0530

    changed config file - 02

--- server/db/index.js
+++ server/db/index.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'database-1.cn28qiwest16.eu-north-1.rds.amazonaws.com',
   user: 'admin',
-  password: 'rohan511',
+  password: 'rohan123',
   database: 'url_shortner',
   waitForConnections: true,
   connectionLimit: 10,
