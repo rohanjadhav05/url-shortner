f57ca7a677549e4291b28deb8a1b9b6991e2b9cb
rohanjadhav05 <rohan.jadhav511@gmail.com>
2024-05-31 22:14:20 +0530

    added the connection details to env file

--- server/db/index.js
+++ server/db/index.js
@@ -1,15 +1,17 @@
 
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: 'database-1.c5gc06sskk9e.ap-south-1.rds.amazonaws.com',
-  user: 'admin',
-  password: 'rohan123',
-  database: 'url_shortner',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  port: '3306',
+  
+  host : process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  waitForConnections: process.env.waitForConnections,
+  connectionLimit: process.env.connectionLimit,
+  queueLimit: process.env.queueLimit,
+  port: process.env.port,
 });
 
 module.exports = pool;
